9a9e797d8105b58c7cb408497aab89137ff22fe8
Jheinember <jheinemberstithjn@ufps.edu.co>
2024-05-16 12:49:30 -0500

    Cambios

--- app/controllers/veterinarios/listado_veterinarios.php
+++ app/controllers/veterinarios/listado_veterinarios.php
@@ -1,27 +1,27 @@
 <?php
 
-try {
-    $sql = "CREATE TABLE IF NOT EXISTS tb_veterinarios (
-        id_veterinario SERIAL PRIMARY KEY,
-        nombre_completo VARCHAR(255),
-        email VARCHAR(255),
-        password TEXT,
-        contacto VARCHAR(50),
-        area_especializacion TEXT,
-        fecha_de_ingreso DATE,
-        fyh_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        fyh_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP        
-    )";
-    
-    $pdo->exec($sql);
-    
-} catch (PDOException $e) {
-    echo "Error al crear la tabla tb_veterinarios: " . $e->getMessage();
-}
+    try {
+        $sql = "CREATE TABLE IF NOT EXISTS tb_veterinarios (
+            id_veterinario SERIAL PRIMARY KEY,
+            nombre_completo VARCHAR(255),
+            email VARCHAR(255),
+            password TEXT,
+            contacto VARCHAR(50),
+            area_especializacion TEXT,
+            fecha_de_ingreso DATE,
+            fyh_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            fyh_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP        
+        )";
+        
+        $pdo->exec($sql);
+        
+    } catch (PDOException $e) {
+        echo "Error al crear la tabla tb_veterinarios: " . $e->getMessage();
+    }
 
-$sql = "SELECT * FROM tb_veterinarios";
-$query = $pdo->prepare($sql);
-$query->execute();
-$veterinarios = $query->fetchAll(PDO::FETCH_ASSOC);
+    $sql = "SELECT * FROM tb_veterinarios";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $veterinarios = $query->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
